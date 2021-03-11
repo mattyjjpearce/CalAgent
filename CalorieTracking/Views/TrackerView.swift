47a19a26773673cbffa8f3401c664fd04d87affc
Matthew Pearce <matthewjjpearce@gmail.com>
2021-03-11 23:31:20 +0000

    If macro progress goes over goal, progressview goes red

--- CalorieTracking/Views/TrackerView.swift
+++ CalorieTracking/Views/TrackerView.swift
@@ -12,17 +12,14 @@ struct TrackerView: View {
     @EnvironmentObject var person: UserInfoModel
     @State private var downloadAmount = 50.0
     @State private var noGoalSet = true
+    @State private var fatColour = Color.blue
 
-    
-    
 
     
     var body: some View {
-            
-       
-
-        
+ 
             NavigationView{
+                
                 
                 if(person.personDailyCalorieGoals.calorieGoal == 0 ||
                     person.personDailyCalorieGoals.carbGoal == 0  ||
@@ -41,26 +38,42 @@ struct TrackerView: View {
                    
                     VStack{
                         
-                        Text("Calories: \(person.personCurrentCalorieProgress.calorieProgress, specifier: "%.0f") kcal")
+                        Text("\(person.personCurrentCalorieProgress.calorieProgress, specifier: "%.0f") / \(person.personDailyCalorieGoals.calorieGoal, specifier: "%.0f") kcal")
                        
                         let CalorieProgress =  (person.personCurrentCalorieProgress.calorieProgress / person.personDailyCalorieGoals.calorieGoal) * 100
-        
-                        ProgressView( value: CalorieProgress, total: 100).scaleEffect(x: 1, y: 3, anchor: .top).shadow(color: .blue, radius: 4.0, x: 1.0, y: 2.0).padding(.bottom, 10)
-                        Text("\(CalorieProgress, specifier: "%.0f")%")
+                        
+                        if(person.personCurrentCalorieProgress.calorieProgress > person.personDailyCalorieGoals.calorieGoal){
+                           
+                            ProgressView( value: CalorieProgress, total: 100).scaleEffect(x: 1, y: 3, anchor: .top).shadow(color: .red, radius: 4.0, x: 1.0, y: 2.0).padding(.bottom, 10)
+                            Text("\(CalorieProgress, specifier: "%.0f")%")
+                        }else{
+                           
+                            ProgressView( value: CalorieProgress, total: 100).scaleEffect(x: 1, y: 3, anchor: .top).shadow(color: .blue, radius: 4.0, x: 1.0, y: 2.0).padding(.bottom, 10)
+                            Text("\(CalorieProgress, specifier: "%.0f")%")
+                            
+                        }
+                        
+                       
                     }.frame(width: .infinity, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                     
 
                 }
                     Section(header: Text("Fat")){
                         VStack{
-                                    Text("Fat: \(person.personCurrentCalorieProgress.fatProgress, specifier: "%.0f")g")
+                            Text("\(person.personCurrentCalorieProgress.fatProgress, specifier: "%.0f")g / \(person.personDailyCalorieGoals.fatGoal, specifier: "%.0f")g")
                                    
                     
             
                             let FatProgress =  (person.personCurrentCalorieProgress.fatProgress / person.personDailyCalorieGoals.fatGoal) * 100
             
-                            ProgressView( value: FatProgress, total: 100).scaleEffect(x: 1, y: 3, anchor: .top).shadow(color: .blue, radius: 4.0, x: 1.0, y: 2.0).padding(.bottom, 10)
-                            Text("\(FatProgress, specifier: "%.0f")%")
+                           
+                            if(person.personCurrentCalorieProgress.fatProgress > person.personDailyCalorieGoals.fatGoal){
+                                ProgressView( value: FatProgress, total: 100).scaleEffect(x: 1, y: 3, anchor: .top).shadow(color: .red, radius: 4.0, x: 1.0, y: 2.0).padding(.bottom, 10)
+                                Text("\(FatProgress, specifier: "%.0f")%")
+                            }else{
+                                ProgressView( value: FatProgress, total: 100).scaleEffect(x: 1, y: 3, anchor: .top).shadow(color: .blue, radius: 4.0, x: 1.0, y: 2.0).padding(.bottom, 10)
+                                Text("\(FatProgress, specifier: "%.0f")%")
+                            }
                         }.frame(width: .infinity, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                         
 
@@ -68,14 +81,22 @@ struct TrackerView: View {
                     
                     Section(header: Text("Carbohydrates")){
                         VStack{
-                                    Text("Carbohydrates: \(person.personCurrentCalorieProgress.carbProgress, specifier: "%.0f")g")
-                                   
+                            Text("\(person.personCurrentCalorieProgress.carbProgress, specifier: "%.0f")g / \(person.personDailyCalorieGoals.carbGoal, specifier: "%.0f")g")
                     
             
                             let CarbProgress =  (person.personCurrentCalorieProgress.carbProgress / person.personDailyCalorieGoals.carbGoal) * 100
-            
-                            ProgressView( value: CarbProgress, total: 100).scaleEffect(x: 1, y: 3, anchor: .top).shadow(color: .blue, radius: 4.0, x: 1.0, y: 2.0).padding(.bottom, 10)
-                            Text("\(CarbProgress, specifier: "%.0f")%")
+                            
+                            if(person.personCurrentCalorieProgress.carbProgress > person.personDailyCalorieGoals.carbGoal){
+                               
+                                ProgressView( value: CarbProgress, total: 100).scaleEffect(x: 1, y: 3, anchor: .top).shadow(color: .red, radius: 4.0, x: 1.0, y: 2.0).padding(.bottom, 10)
+                                Text("\(CarbProgress, specifier: "%.0f")%")
+                            }else{
+                                ProgressView( value: CarbProgress, total: 100).scaleEffect(x: 1, y: 3, anchor: .top).shadow(color: .blue, radius: 4.0, x: 1.0, y: 2.0).padding(.bottom, 10)
+                                Text("\(CarbProgress, specifier: "%.0f")%")
+                                
+                            }
+                            
+                            
                         }.frame(width: .infinity, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                         
 
@@ -83,14 +104,20 @@ struct TrackerView: View {
                     
                     Section(header: Text("Protein")){
                         VStack{
-                                    Text("Protein: \(person.personCurrentCalorieProgress.proteinProgress, specifier: "%.0f")g")
-                                   
+                            Text("\(person.personCurrentCalorieProgress.proteinProgress, specifier: "%.0f")g / \(person.personDailyCalorieGoals.proteinGoal, specifier: "%.0f")g")
                     
             
                             let ProteinProgress =  (person.personCurrentCalorieProgress.proteinProgress / person.personDailyCalorieGoals.proteinGoal) * 100
+                            
+                            if(person.personCurrentCalorieProgress.proteinProgress > person.personDailyCalorieGoals.proteinGoal){
+                                ProgressView( value: ProteinProgress, total: 100).scaleEffect(x: 1, y: 3, anchor: .top).shadow(color: .red, radius: 4.0, x: 1.0, y: 2.0).padding(.bottom, 10)
+                                Text("\(ProteinProgress, specifier: "%.0f")%")
+                            }else{
+                                ProgressView( value: ProteinProgress, total: 100).scaleEffect(x: 1, y: 3, anchor: .top).shadow(color: .blue, radius: 4.0, x: 1.0, y: 2.0).padding(.bottom, 10)
+                                Text("\(ProteinProgress, specifier: "%.0f")%")
+                            }
             
-                            ProgressView( value: ProteinProgress, total: 100).scaleEffect(x: 1, y: 3, anchor: .top).shadow(color: .blue, radius: 4.0, x: 1.0, y: 2.0).padding(.bottom, 10)
-                            Text("\(ProteinProgress, specifier: "%.0f")%")
+                            
                         }.frame(width: .infinity, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                         
 
