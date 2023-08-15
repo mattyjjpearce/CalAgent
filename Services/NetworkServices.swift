eff0ed3e2c568a676d69edcd762c1a5bb45cb692
Matthew Pearce <65558314+mattyjjpearce@users.noreply.github.com>
2023-08-15 21:14:39 +0200

    Update NetworkServices.swift

--- Services/NetworkServices.swift
+++ Services/NetworkServices.swift
@@ -10,34 +10,39 @@ import Alamofire
 
 class NetworkServices {
     
-    //API key to provided by Spoonacular
+    // API key provided by Spoonacular
     static let apiKey: String = "8e8c3bcbd09c402cb0e3279db5db7cc2"
     
-
-    //Function to
+    // Function to fetch recipes based on nutrient constraints
     static func fetchNutrients(maxProtein: Int, maxFat: Int, maxCarbs: Int, number: Int, completionHandler: @escaping (_ response: Any?, _ error: Error?) -> Void) {
         
-        AF.request("https://api.spoonacular.com/recipes/findByNutrients?apiKey=\(NetworkServices.apiKey)&maxFat=\(maxFat)&maxProtein=\(maxProtein)&maxCarbs=\(maxCarbs)&number=\(number)", method: .get).responseJSON { (response) in
-            switch response.result { //Switch statement with result
-            case .success(_): //if successfull
+        // Construct the URL with nutrient constraints and the provided API key
+        let url = "https://api.spoonacular.com/recipes/findByNutrients?" +
+                  "apiKey=\(NetworkServices.apiKey)" +
+                  "&maxFat=\(maxFat)&maxProtein=\(maxProtein)&maxCarbs=\(maxCarbs)&number=\(number)"
+        
+        // Make a GET request to the Spoonacular API using Alamofire
+        AF.request(url, method: .get).responseJSON { (response) in
+            switch response.result { // Switch based on the response result
                 
-                let decoder = JSONDecoder()  //Intantiating a JSONDecoder
+            case .success(_): // If the request is successful
                 
-                    if let data = response.data {
-                        do {
-                            let nutrients = try decoder.decode([RecipeAPI].self, from: data) //Decode the API request and use the RecipeAPI model to place inside variable nutrients
-                            
-                            completionHandler(nutrients, nil) //ensuring it happens 
-                        } catch let error {
-                            completionHandler(nil, error)
-                        }
+                let decoder = JSONDecoder()  // Instantiate a JSONDecoder
+                
+                if let data = response.data {
+                    do {
+                        let nutrients = try decoder.decode([RecipeAPI].self, from: data) // Decode the API response into an array of RecipeAPI using the specified model
+                        
+                        completionHandler(nutrients, nil) // Call the completion handler with the decoded data
+                    } catch let error {
+                        completionHandler(nil, error) // If decoding fails, call the completion handler with an error
                     }
+                }
                 
-            case .failure(let error): //if unsuccessfull
-                completionHandler(nil, error)
+            case .failure(let error): // If the request fails
+                completionHandler(nil, error) // Call the completion handler with an error
             }
         }
     }
   
 }
-
