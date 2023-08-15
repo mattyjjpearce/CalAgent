1694e2e9c554679a238fb6e5644050225f813fb8
Matthew Pearce <65558314+mattyjjpearce@users.noreply.github.com>
2023-08-15 21:12:32 +0200

    Update HealthStore.swift

--- Services/HealthStore.swift
+++ Services/HealthStore.swift
@@ -8,61 +8,63 @@
 import Foundation
 import HealthKit
 
-//Class for all actions related to apple HealthStore
-class HealthStore{
+// Class for all actions related to the Apple HealthStore
+class HealthStore {
     
-    var healthstore: HKHealthStore? //Instantiation a health store
+    var healthstore: HKHealthStore? // Declaration of a variable to hold the HealthStore instance
     
-    
-    init(){ //Whenever this class is used - check if there is availability to the healthstore and if so, create a healthstore variable
-        if HKHealthStore.isHealthDataAvailable(){
-            healthstore = HKHealthStore()
+    init() { // Initialize the HealthStore class
+        
+        if HKHealthStore.isHealthDataAvailable() { // Check if health data is available on the device
+            healthstore = HKHealthStore() // If available, create an instance of HKHealthStore
         }
     }
     
-    
-    
-    func requestAuthorization(completion: @escaping (Bool) -> Void ) {
+    // Request authorization to access health data
+    func requestAuthorization(completion: @escaping (Bool) -> Void) {
         
-        let stepType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)! //Getting the Type of data we want from the health store (step count)
+        let stepType = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)! // Define the type of data to be accessed (step count)
         
-        //unwrapping healthstore so we can get access to an unwrapped version of the healthstore
+        // Unwrap the optional healthstore so we can use it
         guard let healthStore = self.healthstore else { return completion(false) }
         
-        //call request authorization
-        healthStore.requestAuthorization(toShare: [], read: [stepType]){ (success, error) in completion(success)
+        // Request authorization to read step data
+        healthStore.requestAuthorization(toShare: [], read: [stepType]) { (success, error) in
+            completion(success) // Call the completion handler with the authorization result
         }
     }
     
-    //statisticsCollection object contains all steps
+    // Get today's step count
     func getTodaysSteps(completion: @escaping (Double) -> Void) {
        
         let healthStore = self.healthstore
-
-        let stepsQuantityType = HKQuantityType.quantityType(forIdentifier: .stepCount)! //type of data we want (step count)
         
-        let now = Date() //Current time
-        let startOfDay = Calendar.current.startOfDay(for: now) //Start of day
+        let stepsQuantityType = HKQuantityType.quantityType(forIdentifier: .stepCount)! // Define the type of data to be retrieved (step count)
         
-        let predicate = HKQuery.predicateForSamples( //Creating a predicate to use in Query
+        let now = Date() // Get the current time
+        let startOfDay = Calendar.current.startOfDay(for: now) // Calculate the start of the day
+        
+        // Create a predicate to filter data for today
+        let predicate = HKQuery.predicateForSamples(
             withStart: startOfDay,
             end: now,
             options: .strictStartDate
         )
         
-        let query = HKStatisticsQuery( //Querying the healthkit store
+        // Create a query to retrieve cumulative step count
+        let query = HKStatisticsQuery(
             quantityType: stepsQuantityType,
             quantitySamplePredicate: predicate,
             options: .cumulativeSum
         ) { _, result, _ in
-            guard let result = result, let sum = result.sumQuantity() else { //Storing the sum result in variable result 
-                completion(0.0)
+            guard let result = result, let sum = result.sumQuantity() else { // Check if there's a valid result and sum
+                completion(0.0) // If not, call the completion handler with 0 steps
                 return
             }
-            completion(sum.doubleValue(for: HKUnit.count()))
+            completion(sum.doubleValue(for: HKUnit.count())) // Call the completion handler with the step count
         }
         
-        healthStore?.execute(query)
+        healthStore?.execute(query) // Execute the query using the health store instance
     }
     
 }
